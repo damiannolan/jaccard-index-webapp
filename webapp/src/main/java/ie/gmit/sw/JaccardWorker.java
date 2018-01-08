8f7daf56569f64129d4ceba1247209e7098831dd
Damian Nolan <damiannolan@gmail.com>
2018-01-08 00:47:58 +0000

    added jaccard woker class - TODO implement thread functionality using a facade obj for processing doc

---
+++ webapp/src/main/java/ie/gmit/sw/JaccardWorker.java
@@ -0,0 +1,17 @@
+package ie.gmit.sw;
+
+public class JaccardWorker implements Runnable {
+
+	public void run() {
+		// TODO Auto-generated method stub
+		
+		/*
+		 * 1. Create a Jaccard Facade Object
+		 * 2. Compose Jaccard Workers with Facade
+		 * 3. Workers use the Facade to process documents
+		 * 4. Add results to an Out Queue
+		 */
+		
+	}
+
+}
